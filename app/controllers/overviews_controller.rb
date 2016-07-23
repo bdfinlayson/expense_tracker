e3a4331852c0494a3150e93a2b60e5bad3f08a61
Bryan Finlayson <bryan.finlayson@metova.com>
2016-07-22 20:09:26 -0600

    Add budget to overview

--- app/controllers/overviews_controller.rb
+++ app/controllers/overviews_controller.rb
@@ -2,5 +2,7 @@ class OverviewsController < ApplicationController
   def index
     @total_expenses_this_month = current_user.expenses.where(created_at: Time.now.beginning_of_month..Time.now.end_of_month).pluck(:amount).sum
     @categories = current_user.categories.order(name: :asc)
+    @total_for_month = current_user.budgets.pluck(:amount).sum
+    @total_spent = current_user.categories.map(&:summed_expenses).sum
   end
 end
