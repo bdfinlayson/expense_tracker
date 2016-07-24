2546c4ce6f545d0844bb9cab4038f746314c8702
Bryan Finlayson <bryan.finlayson@metova.com>
2016-07-23 22:58:19 -0600

    Fix net worth

--- app/controllers/overviews_controller.rb
+++ app/controllers/overviews_controller.rb
@@ -5,6 +5,6 @@ class OverviewsController < ApplicationController
     @categories = current_user.categories.order(name: :asc)
     @total_for_month = current_user.budgets.pluck(:amount).sum
     @total_spent = current_user.categories.map(&:summed_expenses).sum
-    @net_worth_for_month = @total_expenses_this_month - @income_this_month
+    @net_worth_for_month = @income_this_month - @total_expenses_this_month
   end
 end
