9252caf4e0b074ca04becfa53b774eb60b552acd
Bryan Finlayson <bdfinlayson@gmail.com>
2016-09-10 08:12:23 -0500

    rearrange lib files, add spec directory

--- test/controllers/pending_expenses_controller_test.rb
+++
@@ -1,9 +0,0 @@
-require 'test_helper'
-
-class PendingExpensesControllerTest < ActionDispatch::IntegrationTest
-  test "should get index" do
-    get pending_expenses_index_url
-    assert_response :success
-  end
-
-end
--- test/models/recurring_expense_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class RecurringExpenseTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-end
