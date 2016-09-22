d23bdec96fe42d0c040db9e5bdb28ded2ecf4fdd
Bryan Finlayson <bryan.finlayson@metova.com>
2016-09-22 11:19:04 -0500

    Add profit specs

--- app/controllers/overviews_controller.rb
+++ app/controllers/overviews_controller.rb
@@ -4,6 +4,8 @@ class OverviewsController < ApplicationController
   def index
     @total_expenses_this_month = current_user.expenses.where(created_at: Time.now.beginning_of_month..Time.now.end_of_month).pluck(:amount).sum
     @monthly_expenses_chart = get_monthly_expense_history
+    @monthly_income_history = get_monthly_income_history
+    @monthly_profit_history = get_monthly_profit_history(@monthly_expenses_chart.values, @monthly_income_history.values)
     @income_this_month = current_user.incomes.where(created_at: Time.now.beginning_of_month..Time.now.end_of_month).pluck(:amount).sum
     @categories = current_user.expense_categories.order('lower(name) asc')
     @total_for_month = current_user.budgets.pluck(:amount).sum
@@ -33,6 +35,16 @@ class OverviewsController < ApplicationController
     months.shift
     months.each_with_index do |month, i|
       data.merge!(month => @incomes.where('extract(month from created_at) = ?', i + 1).sum(:amount))
+    end
+    data
+  end
+
+  def get_monthly_profit_history(expenses, incomes)
+    data = {}
+    months = Date::MONTHNAMES.dup
+    months.shift
+    months.each_with_index do |month, i|
+      data.merge!(month => expenses[i] - incomes[i])
     end
     data
   end
--- spec/controllers/overviews_controller_spec.rb
+++ spec/controllers/overviews_controller_spec.rb
@@ -59,4 +59,51 @@ describe OverviewsController, type: :controller do
       expect(h['October']).to eq 0.0
     end
   end
+
+  describe '#get_monthly_profit_history' do
+    before do
+      Timecop.freeze(Date.new(2016,9,22)) do
+        # expenses
+        5.times do
+          FactoryGirl.create(:expense, user: user, vendor: vendor, expense_category: expense_category, amount: 1000, created_at: Time.now.months_ago(3))
+        end
+        4.times do
+          FactoryGirl.create(:expense, user: user, vendor: vendor, expense_category: expense_category, amount: 500, created_at: Time.now.months_ago(2))
+        end
+        3.times do
+          FactoryGirl.create(:expense, user: user, vendor: vendor, expense_category: expense_category, amount: 1000, created_at: Time.now.months_ago(1))
+        end
+        2.times do
+          FactoryGirl.create(:expense, user: user, vendor: vendor, expense_category: expense_category, amount: 1000, created_at: Time.now)
+        end
+
+        # incomes
+        5.times do
+          FactoryGirl.create(:income, user: user, vendor: vendor, income_category: income_category, amount: 500, created_at: Time.now.months_ago(3))
+        end
+        4.times do
+          FactoryGirl.create(:income, user: user, vendor: vendor, income_category: income_category, amount: 1000, created_at: Time.now.months_ago(2))
+        end
+        3.times do
+          FactoryGirl.create(:income, user: user, vendor: vendor, income_category: income_category, amount: 500, created_at: Time.now.months_ago(1))
+        end
+        2.times do
+          FactoryGirl.create(:income, user: user, vendor: vendor, income_category: income_category, amount: 500, created_at: Time.now)
+        end
+      end
+      sign_in user
+    end
+
+    it 'returns montly profit' do
+      expenses = controller.get_monthly_expense_history.values
+      incomes = controller.get_monthly_income_history.values
+      h = controller.get_monthly_profit_history(expenses, incomes)
+      expect(h['May']).to eq 0.0
+      expect(h['June']).to eq 2500.0
+      expect(h['July']).to eq -2000.0
+      expect(h['August']).to eq 1500.0
+      expect(h['September']).to eq 1000.0
+      expect(h['October']).to eq 0.0
+    end
+  end
 end
