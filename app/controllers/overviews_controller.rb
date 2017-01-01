88cb4c9b8f88d34a19eb92620757096be7891aef
Bryan Finlayson <bryan.finlayson@metova.com>
2017-01-01 11:49:22 -0600

    update layout, remove comments

--- app/controllers/overviews_controller.rb
+++ app/controllers/overviews_controller.rb
@@ -6,12 +6,10 @@ class OverviewsController < ApplicationController
     @last_year = @year.to_i - 1
     @next_year = @year.to_i + 1
     @total_expenses_this_month = current_user.expenses.where('extract(year from created_at) = ?', @year).where('extract(month from created_at) = ?', Time.now.month).pluck(:amount).sum
-    # (created_at: Time.now.beginning_of_month..Time.now.end_of_month).pluck(:amount).sum
     @monthly_expenses_chart = get_monthly_expense_history
     @monthly_income_history = get_monthly_income_history
     @monthly_profit_history = get_monthly_profit_history(@monthly_expenses_chart.values, @monthly_income_history.values)
     @income_this_month = current_user.incomes.where('extract(year from created_at) = ?', @year).where('extract(month from created_at) = ?', Time.now.month).pluck(:amount).sum
-    #.where(created_at: Time.now.beginning_of_month..Time.now.end_of_month).pluck(:amount).sum
     @categories = current_user.expense_categories.order('lower(name) asc')
     @total_for_month = current_user.budgets.pluck(:amount).sum
     @total_spent = current_user.expense_categories.map(&:summed_expenses).sum
